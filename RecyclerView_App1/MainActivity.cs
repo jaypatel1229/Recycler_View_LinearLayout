a77025088f09915f4849715a5a8940bd91b0b86e
jay.patel <jay.patel@1R-80>
2022-02-23 12:32:51 +0530

    Add project files.

---
+++ RecyclerView_App1/MainActivity.cs
@@ -0,0 +1,106 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace RecyclerView_App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private RecyclerView recyclerView;
+        private ListView _NameList;
+        private List<string> _nameList;
+        private NameAdapter _nameAdapter;
+        RecyclerView.LayoutManager layoutManager;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            recyclerView = FindViewById<RecyclerView>(Resource.Id._nameRecyclerView);
+            _NameList = FindViewById<ListView>(Resource.Id._nameList);
+
+            GenerateDumyRecord();
+           
+
+            layoutManager = new LinearLayoutManager(this);
+            recyclerView.SetLayoutManager(layoutManager);
+            _nameAdapter = new NameAdapter(_nameList);
+
+            recyclerView.SetAdapter(_nameAdapter);
+            _nameAdapter.ItemClick += _nameAdapterItem_Click;
+
+
+        }
+        private void _nameAdapterItem_Click(object sender,int i)
+        {
+            Toast.MakeText(this, "Hii" +_nameList[i].ToString(),ToastLength.Short).Show();
+        }
+
+        private void GenerateDumyRecord()
+        {
+            _nameList = new List<string>();
+
+            _nameList.Add("Hinal");
+            _nameList.Add("Vruti");
+            _nameList.Add("Aman");
+            _nameList.Add("Rakul");
+            _nameList.Add("Prit");
+            _nameList.Add("Manish");
+            _nameList.Add("Hiral");
+            _nameList.Add("Hinal");
+            _nameList.Add("Vruti");
+            _nameList.Add("Aman");
+            _nameList.Add("Rakul");
+            _nameList.Add("Prit");
+            _nameList.Add("Manish");
+            _nameList.Add("Hiral");
+            _nameList.Add("Hinal");
+            _nameList.Add("Vruti");
+            _nameList.Add("Aman");
+            _nameList.Add("Rakul");
+            _nameList.Add("Prit");
+            _nameList.Add("Manish");
+            _nameList.Add("Hiral");
+            _nameList.Add("Hinal");
+            _nameList.Add("Vruti");
+            _nameList.Add("Aman");
+            _nameList.Add("Rakul");
+            _nameList.Add("Prit");
+            _nameList.Add("Manish");
+            _nameList.Add("Hiral");
+            _nameList.Add("Hinal");
+            _nameList.Add("Vruti");
+            _nameList.Add("Aman");
+            _nameList.Add("Rakul");
+            _nameList.Add("Prit");
+            _nameList.Add("Manish");
+            _nameList.Add("Hiral");
+            _nameList.Add("Hinal");
+            _nameList.Add("Vruti");
+            _nameList.Add("Aman");
+            _nameList.Add("Rakul");
+            _nameList.Add("Prit");
+            _nameList.Add("Manish");
+            _nameList.Add("Hiral");
+
+            //foreach (string name in _nameList)
+            //    _nameList.Add(name);
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ RecyclerView_App1/NameAdapter.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecyclerView_App1 
+{
+    
+    class NameAdapter : RecyclerView.Adapter
+    {
+        private List<string> _nameList;
+        private int position;
+
+        public event EventHandler<int> ItemClick;
+        public NameAdapter(List<string> Name)
+        {
+            _nameList = Name;
+        }
+        public override int ItemCount => _nameList.Count;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            NameViewHolder nameViewHolder = holder as NameViewHolder;
+            nameViewHolder.BindData(_nameList[position]);
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout._newname_listrow_item,parent,false);
+            return new NameViewHolder(view, OnClick);
+        }
+        public void OnClick(int view)
+        {
+            if (ItemClick != null)
+            {
+                ItemClick?.Invoke(this, position);
+            }
+            ItemClick?.Invoke(this, position);
+        }
+    }
+
+    class NameViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView nameText;
+        public NameViewHolder(View itemView,Action<int> listner):base(itemView)
+        {
+            nameText = itemView.FindViewById<TextView>(Resource.Id._nameText);
+            nameText.Click += (sender, e) => listner(base.LayoutPosition);
+            
+        }
+
+        internal void BindData(string Name)
+        {
+            nameText.Text = Name;
+        }
+    }
+}
